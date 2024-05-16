d81ff1b4e38441129b7a298c5e8f963a2b1421e6
TheuZCoder <matheusrdsilva1@gmail.com>
2024-05-16 19:50:13 -0300

    Fix: Removido espaços em branco no codigo para melhor leitura

--- src/main/java/matheus/api/api/Service/UsuarioService.java
+++ src/main/java/matheus/api/api/Service/UsuarioService.java
@@ -2,10 +2,8 @@ package matheus.api.api.Service;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import matheus.api.api.Model.Usuario;
 import matheus.api.api.Repository.UsuarioRepository;
 
