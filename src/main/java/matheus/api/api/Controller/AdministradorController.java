b8b1f57424bc409977510707387a4d7b276f3831
TheuZCoder <matheusrdsilva1@gmail.com>
2024-05-16 16:57:51 -0300

    Create: Feito Controllers até o Bloco Patrimonio

--- src/main/java/matheus/api/api/Controller/AdministradorController.java
+++ src/main/java/matheus/api/api/Controller/AdministradorController.java
@@ -1,9 +1,19 @@
 package matheus.api.api.Controller;
 
+import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import matheus.api.api.Model.Administrador;
 import matheus.api.api.Service.AdministradorService;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("/administrador")
@@ -11,4 +21,31 @@ public class AdministradorController {
 
     @Autowired
     private AdministradorService administradorService;
+
+    @GetMapping
+    public List<Administrador> getAllAdministradores(){
+        return administradorService.findAll();
+    }
+
+    @GetMapping("/{id_administrador}")
+    public ResponseEntity<Administrador> getAdministradorById(@PathVariable Integer id_administrador){
+        Optional<Administrador> administrador = administradorService.findById(id_administrador);
+        return administrador.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Administrador createAdministrador(@RequestBody Administrador administrador){
+        return administradorService.save(administrador);
+    }
+    
+    /*@PutMapping("/{id_administrador}")
+    public ResponseEntity<Administrador> updateAdministrador(@PathVariable Integer id_administrador ){
+        
+    }         AJUSTAR CODIGO DE ATUALIZAÇÃO QUANDO OBTER O BANCO JA MODELADO*/
+    
+    @DeleteMapping("/{id_administrador}")
+    public ResponseEntity<Void> deleteAdministrador(@PathVariable Integer id_administrador){
+        administradorService.deleteById(id_administrador);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/matheus/api/api/Controller/BlocoPatrimonioController.java
+++ src/main/java/matheus/api/api/Controller/BlocoPatrimonioController.java
@@ -1,9 +1,23 @@
 package matheus.api.api.Controller;
 
+import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import matheus.api.api.Model.BlocoPatrimonio;
 import matheus.api.api.Service.BlocoPatrimonioService;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
 
 @RestController
 @RequestMapping("/blocopatrimonio")
@@ -11,4 +25,32 @@ public class BlocoPatrimonioController {
     
     @Autowired
     private BlocoPatrimonioService blocoPatrimonioService;
+
+    @GetMapping
+    public List<BlocoPatrimonio> getAllBlocoPatrimonio(){
+        return blocoPatrimonioService.findAll();
+    }
+    
+    @GetMapping("/id_bloco_patrimonio")
+    public ResponseEntity<BlocoPatrimonio> getBlocoPatrimonioById(@PathVariable Integer id_bloco_patrimonio){
+        Optional<BlocoPatrimonio> blocoPatrimonio = blocoPatrimonioService.findById(id_bloco_patrimonio);
+        return blocoPatrimonio.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public BlocoPatrimonio createBlocoPatrimonio(@RequestBody BlocoPatrimonio blocoPatrimonio){
+        return blocoPatrimonioService.save(blocoPatrimonio);
+    }
+
+    /*
+    @PutMapping("/{id_bloco_patrimonio}")
+    public ResponseEntity<BlocoPatrimonio> updateBlocoPatrimonio(@PathVariable)
+    }  AJUSTAR CODIGO DE ATUALIZAÇÃO QUANDO OBTER O BANCO JA MODELADO*/
+    
+
+    @DeleteMapping("/{id_bloco_patrimonio}")
+    public ResponseEntity<Void> deleteAdministrador(@PathVariable Integer id_bloco_patrimonio){
+        blocoPatrimonioService.deleteById(id_bloco_patrimonio);
+        return ResponseEntity.noContent().build();
+    }
 }
